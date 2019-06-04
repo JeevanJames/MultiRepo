51d1369e254e05dc2b02bb0809604246a0e09fdd
Jeevan James <jeevanjj@live.com>
2019-06-05 03:59:57 +0530

    Add "tags list" command.

---
+++ src/Core/Commands/TagsCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using ConsoleFx.CmdLine;
+
+namespace Core.Commands
+{
+    [Command("tags")]
+    public sealed class TagsCommand : Command
+    {
+    }
+
+    [Command("list", typeof(TagsCommand))]
+    public sealed class TagsListCommand : RepoCommand
+    {
+        protected override int HandleCommand()
+        {
+            IEnumerable<string> allTags = FilteredRepositories
+                .SelectMany(defn => defn.Value.Tags)
+                .Distinct(StringComparer.OrdinalIgnoreCase)
+                .OrderBy(tag => tag);
+            foreach (string tag in allTags)
+                Console.WriteLine(tag);
+            return 0;
+        }
+    }
+}
