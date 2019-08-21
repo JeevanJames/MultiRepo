12c204f01e10084b5dd40500fbf7f2b5136fe006
Jeevan James <jeevanjj@live.com>
2019-08-21 15:13:53 +0530

    Add root command to change to project root dir

---
+++ src/Core/Commands/RootCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+using ConsoleFx.CmdLine;
+
+namespace Core.Commands
+{
+    [Command("root")]
+    public sealed class RootCommand : ManifestCommand
+    {
+        protected override int HandleCommand()
+        {
+            Directory.SetCurrentDirectory(Project.RootDirectory.FullName);
+            return 0;
+        }
+    }
+}
