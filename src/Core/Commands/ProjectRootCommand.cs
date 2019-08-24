226fbefeec9fd84002c85c8d798c3a40b671b641
Jeevan James <jeevanjj@live.com>
2019-08-24 17:16:47 +0530

    Add root command as project root command

---
+++ src/Core/Commands/ProjectRootCommand.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleFx.CmdLine;
+
+using Core.Vcs.Commands;
+
+using static ConsoleFx.ConsoleExtensions.Clr;
+using static ConsoleFx.ConsoleExtensions.ConsoleEx;
+
+namespace Core.Commands
+{
+    [Command("root", ParentType = typeof(ProjectCommand))]
+    public sealed class ProjectRootCommand : ManifestCommand
+    {
+        protected override int HandleCommand()
+        {
+            PrintLine(Project.RootDirectory.FullName);
+            return 0;
+        }
+    }
+}
